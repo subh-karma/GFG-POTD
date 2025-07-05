78f1d62b97d59dd26adb815bdb72f1477181a80f
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2025-07-05 21:20:28 +0530

    Create 5july25.py

---
+++ 5july25.py
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int maxSum(vector<int> &arr) {
+        int res=0;
+        for(int i=0;i<arr.size()-1;i++)
+        {
+            res=max(res,arr[i]+arr[i+1]);
+        }
+        return res;
+        // code here
+        
+    }
+};
