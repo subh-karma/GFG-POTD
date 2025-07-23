5b820d7c77c8a1e44face925edb0d1c302b38ee7
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2025-07-23 21:04:16 +0530

    Create 23july25.py

---
+++ 23july25.py
@@ -0,0 +1,9 @@
+class Solution {
+    public int subarraySum(int[] arr) {
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i]*(i+1)*(arr.length-i);
+        }
+        return sum;
+    }
+}
