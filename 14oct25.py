ee33ed5a90ce20dd4d4943e99fbb90a9d0d53ba8
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2025-10-14 21:52:11 +0530

    Create 14oct25.py

---
+++ 14oct25.py
@@ -0,0 +1,29 @@
+"""
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+"""
+
+class Solution:
+    def nodeSum(self, root, l, r):
+        found=False
+        ret=0
+        def dfs(cur=root):
+            nonlocal l,r,found,ret
+            if not cur:
+                return
+            dfs(cur.left)
+            if l<=cur.data<=r:
+                found=True
+                ret+=cur.data
+            if found and (cur.data<l or cur.data>r):
+                return
+            dfs(cur.right)
+        dfs()
+        return ret
+
+
+        
+        
