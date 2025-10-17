8e4b02e0b4e72429ffcf97fdb2ee8b9d7f159aca
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2025-10-17 22:17:08 +0530

    17oct25.py

---
+++ 17oct25.py
@@ -0,0 +1,26 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};*/
+
+class Solution {
+    int cs = 0;
+  public:
+    void transformTree(Node *root) {
+        if(!root)return;
+        int d = root->data;
+        transformTree(root->right);
+        root->data = cs;
+        cs += d;
+        transformTree(root->left);
+    }
+};
