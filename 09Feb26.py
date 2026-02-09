90c5dcf7041099e7a811a2e146a62b5b5e1eecfc
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2026-02-09 21:17:40 +0530

    Add findKRotation method to Solution class
    Implement findKRotation method to find rotation count.

---
+++ 09Feb26.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findKRotation(self, arr):
+        sortedArr=sorted(arr)
+        maxEle=max(arr)
+        if sortedArr!=arr:
+            ans=arr.index(maxEle)
+            return  ans+1
+        return 0
+        # code here
+        
