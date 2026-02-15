9ccb35779d512c6134e75ac7341fd5e54e0f6cc2
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2026-02-15 14:52:32 +0530

    Add findMinDiff function to calculate min difference
    Implement findMinDiff function to find minimum difference.

---
+++ 15Feb26.py
@@ -0,0 +1,15 @@
+#User function Template for python3
+
+class Solution:
+    def findMinDiff(self, arr,M):
+        lth=len(arr)
+        arr.sort()
+        mn=float('inf')
+        for left,ve in enumerate(arr):
+            right=left+M-1
+            if right>=lth:
+                break
+            mn=min(mn,arr[right]-arr[left])
+        return mn
+
+        # code here
