28e3b9fd9417d4e97b34553b41d83629276e46ed
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2026-03-02 21:38:34 +0530

    Create 02March26.py

---
+++ 02March26.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxWater(self, arr):
+        # code here
+        n = len(arr)
+        if n < 3:
+            return 0
+    
+        l, r = 0, n - 1
+        leftMax = rightMax = 0
+        water = 0
+    
+        while l <= r:
+            if arr[l] <= arr[r]:
+                if arr[l] >= leftMax:
+                    leftMax = arr[l]
+                else:
+                    water += leftMax - arr[l]
+                l += 1
+            else:
+                if arr[r] >= rightMax:
+                    rightMax = arr[r]
+                else:
+                    water += rightMax - arr[r]
+                r -= 1
+    
+        return water
+        # code here
+        
