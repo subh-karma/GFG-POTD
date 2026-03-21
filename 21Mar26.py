b821dcd171322777e67243126dc8afafce13a915
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2026-03-21 21:30:29 +0530

    Implement countBSTs method to count BSTs from array

---
+++ 21Mar26.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countBSTs(self, arr):
+        n = len(arr)
+        
+        catalan = [0] * 7
+        catalan[0] = catalan[1] = 1
+        
+        for i in range(2, 7):
+            for j in range(i):
+                catalan[i] += catalan[j] * catalan[i - j - 1]
+        
+        result = []
+        
+        for i in range(n):
+            root = arr[i]
+            left = sum(1 for x in arr if x < root)
+            right = sum(1 for x in arr if x > root)
+            
+            result.append(catalan[left] * catalan[right])
+        
+        return result
+        # Code here
