bd9d5665cfe291b42a32a081afb0399d06835518
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2026-04-12 19:38:45 +0530

    Add Toeplitz matrix check function
    Implement function to check if a matrix is Toeplitz.

---
+++ 12Apr26.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isToeplitz(self, mat):
+        rows = len(mat)
+        cols = len(mat[0])
+        
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if mat[i][j] != mat[i-1][j-1]:
+                    return False
+        
+        return True
+
+
+        # code here
+        
