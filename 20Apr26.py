026c06bb8c71a26021b6b9ea8cbaf76d1135c7ec
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2026-04-20 21:31:05 +0530

    Add derangeCount method to calculate derangements
    Implement derangement count calculation using dynamic programming.

---
+++ 20Apr26.py
@@ -0,0 +1,18 @@
+class Solution:
+    def derangeCount(self, n: int) -> int:
+        if n==1:
+            return 0
+        
+        if n==2:
+            return 1
+        
+        dp = [0]*(n+1)
+        
+        dp[1] = 0
+        dp[2] = 1
+        
+        for i in range(3,n+1):
+            dp[i] = (i-1)*(dp[i-1]+dp[i-2])
+        
+        return dp[n]
+        # code here
