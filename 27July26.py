a761ad7599bafdbf5fbe9749551fb0233aadb5b4
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2026-07-27 21:17:49 +0530

    Add binary tree construction from preorder traversal
    Implemented a solution to construct a binary tree from preorder and its mirror.

---
+++ 27July26.py
@@ -0,0 +1,28 @@
+''' Structure of Binary Tree Node
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.right = None
+        self.left = None
+'''
+
+class Solution:
+    def constructBinaryTree(self, pre, preMirror):
+        i, j = 0, len(pre) - 1
+
+        def build() -> Node:
+            nonlocal i, j
+            node = Node(pre[i])
+            if pre[i] == preMirror[j]:
+                i += 1
+                j -= 1
+            else:
+                i += 1
+                node.left = build()
+                node.right = build()
+                j -= 1
+            return node
+
+        return build()
+        # code here
+        
