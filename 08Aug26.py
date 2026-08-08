2ffc038abc80663db204dbded580abeed324f420
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2026-08-08 12:35:31 +0530

    Create 08Aug26.py

---
+++ 08Aug26.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEdgesReq(self, n, edges):
+        if len(edges) < n - 1:
+            return -1
+        parent = list(range(n))
+        def find(u: int) -> int:
+            if parent[u] != u:
+                parent[u] = find(parent[u])
+            return parent[u]
+        components = n
+        for u, v in edges:
+            u, v = find(u), find(v)
+            if u != v:
+                parent[u] = v
+                components -= 1
+        return components - 1
+
+
+        # code here
+        
