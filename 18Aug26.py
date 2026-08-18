8d7973d6012df3932efa0348aa3a13368329c990
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2026-08-18 08:03:18 +0530

    Add string compression method to Solution class
    Implement a string compression method using a stack.

---
+++ 18Aug26.py
@@ -0,0 +1,16 @@
+class Solution:
+    def compress(self, s):
+        # code here
+    
+        n = len(s)
+        stack = []
+        while n > 0:
+            if s[:n//2] == s[n//2:n]:
+                stack.append('*')
+                n //= 2
+            else:
+                n -= 1
+                stack.append(s[n])
+        return "".join(reversed(stack))
+            # code here
+        
